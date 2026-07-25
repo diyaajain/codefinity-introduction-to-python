83041356c1bd83f3304ca568210b5af17e278104
Diya Jain <82181956+diyaajain@users.noreply.github.com>
2026-07-25 12:23:18 +0530

    Update favorite_language.py

--- python-crash-course/chapter-6/favorite_language.py
+++ python-crash-course/chapter-6/favorite_language.py
@@ -17,3 +17,11 @@ favorite_languages = {
 
 for name in favorite_languages.keys():
     print(name.title())
+
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(name.title())
+
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}!")
