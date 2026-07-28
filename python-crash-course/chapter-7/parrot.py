c09db4302d56eab98ac746299c79c743c9647d29
Diya Jain <82181956+diyaajain@users.noreply.github.com>
2026-07-28 11:01:21 +0530

    Update parrot.py

--- python-crash-course/chapter-7/parrot.py
+++ python-crash-course/chapter-7/parrot.py
@@ -3,3 +3,11 @@ print(message)
 
 name = input("Please enter your name: ")
 print(f"\nHello, {name}!")
+
+prompt = "If you tell us who you are, we can personalize the messages you see."
+prompt += "\nWhat is your first name? "
+first_name = input(prompt)
+prompt = "What is your last name? "
+last_name = input(prompt)
+full_name = f"{first_name} {last_name}"
+print(f"\nHello, {full_name.title()}!")
