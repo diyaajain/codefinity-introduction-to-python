700dfa0bfdde9c2454edd032bd31a3a424271753
Diya Jain <82181956+diyaajain@users.noreply.github.com>
2026-07-28 11:25:20 +0530

    Update parrot.py

--- python-crash-course/chapter-7/parrot.py
+++ python-crash-course/chapter-7/parrot.py
@@ -11,3 +11,10 @@ prompt = "What is your last name? "
 last_name = input(prompt)
 full_name = f"{first_name} {last_name}"
 print(f"\nHello, {full_name.title()}!")
+
+number = input("Enter a number, and I will tell you if it's even or odd: ")
+number = int(number)
+if number % 2 == 0:
+    print(f"\nThe number {number} is even.")
+else:
+    print(f"\nThe number {number} is odd.")
