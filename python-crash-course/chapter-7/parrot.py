8532dfa24ce46143b47c68db3b439d0729900380
Diya Jain <82181956+diyaajain@users.noreply.github.com>
2026-07-28 11:28:22 +0530

    Update parrot.py

--- python-crash-course/chapter-7/parrot.py
+++ python-crash-course/chapter-7/parrot.py
@@ -18,3 +18,11 @@ if number % 2 == 0:
     print(f"\nThe number {number} is even.")
 else:
     print(f"\nThe number {number} is odd.")
+
+prompt = "\nTell me something, and I will repeat it back to you."
+prompt += "\nEnter 'quit' to end the program. "
+message = ""
+while message != 'quit':
+    message = input(prompt)
+    if message != 'quit':
+        print(message)
